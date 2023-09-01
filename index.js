f94b48e601b0953e48fae6e28c98947fc8679c46
unknown <sourabh.rajput.22082001@gmail.com>
2023-09-01 07:41:28 +0530

    add Data to start working

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 require("dotenv").config();
+const {UserRouter} = require("./Routes/UserRoute")
+const {CartRouter} = require("./Routes/CartRouter")
+const {ProductRouter} = require("./Routes/ProductRoute")
+const {connection} = require("./config/db")
 
 const app = express();
+
+app.use(express.json());
+app.use("/users",UserRouter)
+app.use("/products",ProductRouter)
+app.use("/cart",CartRouter);
+
+
 app.get("/",(req,res)=>{
     res.send("home page")
 })
 
 app.listen(process.env.PORT,async()=>{
     try {
-        // await connection
+        await connection
         console.log("started the server");
     } catch (error) {
         console.log(error.message)
